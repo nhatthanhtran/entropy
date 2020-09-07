3f9b732bb71cc22fbc6f69252888c4ced0ce74ad
nhatthanhtran <67355006+nhatthanhtran@users.noreply.github.com>
2020-09-07 14:37:27 -0700

    Delete ExactSolution1D.m

--- ExactSolution1D.m
+++
@@ -1,6 +0,0 @@
-function vecExactSolution = ExactSolution1D(vecX, T, D, V, xCenter)
-%exact solution of the 1D diffusion eq
-    funExactSolution = @(x) 1/sqrt(4*pi*D*T).*exp(-(abs(x - V*T - xCenter).^2)/(4*D*T));
-    vecExactSolution = feval(funExactSolution, vecX);
-end
-
